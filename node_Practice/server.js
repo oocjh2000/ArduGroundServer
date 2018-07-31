ae56ea4b71d654ec7e21b98434197a9baa5b6a5c
DESKTOP-27VVUBG\chats <oocjh200@gmail.com>
2018-07-31 11:06:15 +0900

    .

--- node_Practice/server.js
+++ node_Practice/server.js
@@ -1,56 +1,43 @@
-﻿const express = require('express');
-const app = express();
-
-let users = [
+var express = require('express');
+var app = express();
+var users = [
     { id: 1, name: 'cha' },
     { id: 2, name: 'kim' },
     { id: 3, name: 'rtr' }
 ];
-
 function User(_name) {
     this.name = _name;
 }
-
 User.prototype.greeting = function () {
     console.log('Hello! ');
     return this;
 };
-
 User.prototype.introduce = function () {
-    console.log(`I am ${this.name}`);
+    console.log("I am " + this.name);
     return this;
 };
-
 var chris = new User('chris');
-
 chris.greeting().introduce();
-
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
     res.send('h/w');
 });
-
-app.listen(3000, () => {
+app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
-
-app.get('/users', (req, res) => {
+app.get('/users', function (req, res) {
     res.json(users);
     console.log('asdf');
 });
-
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', function (req, res) {
     console.log(req.params.id);
-    const id = parseInt(req.params.id, 10);
-
+    var id = parseInt(req.params.id, 10);
     if (!id) {
         return res.status(400).json({ error: 'inocerrect id' });
     }
-
-    let user = users.filter(user => { return user.id === id; })[0]
-
+    var user = users.filter(function (user) { return user.id === id; })[0];
     if (!user) {
         return res.status(404).json({ error: 'unknown user' });
     }
-
     return res.json(user);
 });
+//# sourceMappingURL=server.js.map

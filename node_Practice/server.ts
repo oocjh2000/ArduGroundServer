4939c78ad195024fa39f1acd6fe4f69e7df28fcb
DESKTOP-27VVUBG\chats <oocjh200@gmail.com>
2018-08-03 10:38:00 +0900

    초기화 추가 / 리팩터링

--- node_Practice/server.ts
+++ node_Practice/server.ts
@@ -1,72 +1,15 @@
 ﻿const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-let users = [
-    { id: 1, name: 'cha' },
-    { id: 2, name: 'kim' },
-    { id: 3, name: 'rtr' }
-];
-
-
+app.use('/users', require('./users'));
 
 app.get('/', (req, res) => {
     res.send('유종효특: 병신tv봄 얼라이언스임 시키는거 안함\n' + res.connection.localPort);
 });
 
 app.listen(80, () => {
-    console.log('Example app listening');
-});
-
-app.get('/users', (req, res) => {
-    res.json(users);
-    console.log('asdf');
-});
-
-app.get('/users/:id', (req, res) => {
-    console.log(req.params.id);
-    const id = parseInt(req.params.id, 10);
-
-    if (!id) {
-        return res.status(400).json({ error: 'inocerrect id' });
-    }
-
-    let user = users.filter(user => { return user.id === id; })[0]
-
-    if (!user) {
-        return res.status(404).json({ error: 'unknown user' });
-    }
-
-    return res.json(user);
-});
-
-app.delete('/users/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    if (!id) {
-        return res.status(400).json({ error: 'Incorrect id' });
-    }
-
-    const userIdx = users.findIndex(user => user.id === id);
-    if (userIdx === -1) {
-        return res.status(404).json({ error: 'Unknown user' });
-    }
-
-    users.splice(userIdx, 1);
-    res.status(204).send();
+    console.log('Example router listening');
 });
 
 
 
-app.post('/users', (req, res) => {
-
-    const name = req.body.name || '';
-    if (!name.length) return res.status(400).json({ error: 'incorrenct name' });
-    const id = users.reduce((maxId, user) => { return user.id > maxId ? user.id : maxId }, 0) + 1;
-    const newUser = { id: id, name: name };
-    users.push(newUser);
-    return res.status(201).json(newUser);
-
-});
---
+++ node_Practice/users.js
@@ -0,0 +1,53 @@
+const Express = require('express');
+const bodyParser = require('body-parser');
+const router = Express.Router();
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+let users = [
+    { id: 0, name: 'test' }
+];
+router.delete('/reset', (req, res) => {
+    users = [
+        { id: 0, name: 'test' }
+    ];
+    res.status(200).send(users).json;
+});
+router.get('/', (req, res) => {
+    res.json(users);
+    console.log('asdf');
+});
+router.get('/:id', (req, res) => {
+    console.log(req.params.id);
+    const id = parseInt(req.params.id, 10);
+    if (!id) {
+        return res.status(400).json({ error: 'inocerrect id' });
+    }
+    let user = users.filter(user => { return user.id === id; })[0];
+    if (!user) {
+        return res.status(404).json({ error: 'unknown user' });
+    }
+    return res.json(user);
+});
+router.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    if (!id) {
+        return res.status(400).json({ error: 'Incorrect id' });
+    }
+    const userIdx = users.findIndex(user => user.id === id);
+    if (userIdx === -1) {
+        return res.status(404).json({ error: 'Unknown user' });
+    }
+    users.splice(userIdx, 1);
+    res.status(204).send();
+});
+router.post('/', (req, res) => {
+    const name = req.body.name || '';
+    if (!name.length)
+        return res.status(400).json({ error: 'incorrenct name' });
+    const id = users.reduce((maxId, user) => { return user.id > maxId ? user.id : maxId; }, 0) + 1;
+    const newUser = { id: id, name: name };
+    users.push(newUser);
+    return res.status(201).json(newUser);
+});
+module.exports = router;
+//# sourceMappingURL=users.js.map
